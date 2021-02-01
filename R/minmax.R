e68251b41dd0f1d0965e8ebae04c16c4fa21bc8e
HughParsonage <hugh.parsonage@gmail.com>
2021-02-02 01:34:05 +1100

    Expand coverage, clarify empty_result

--- R/minmax.R
+++ R/minmax.R
@@ -1,11 +1,14 @@
 #' Minimum and maximum 
 #' 
 #' @param x A vector for which the minimum and maximum are desired.
+#' @param empty_result The return value if \code{length(x) == 0}.
 #' @param nThread An integer, the number of threads to use.
 #' 
+#' @return A length-two vector, the minimum and maximum of \code{x}. If 
+#' \code{typeof(x)} is not supported, \code{NULL}.
 #' 
 #' @export
 
-minmax <- function(x, nThread = getOption("hutilsc.nThread", 1L)) {
-  .Call("do_minmax", x, x[0], nThread, PACKAGE = packageName())
+minmax <- function(x, empty_result = NULL, nThread = getOption("hutilsc.nThread", 1L)) {
+  .Call("do_minmax", x, empty_result, nThread, PACKAGE = packageName())
 }
--- inst/tinytest/test_minmax.R
+++ inst/tinytest/test_minmax.R
@@ -6,5 +6,8 @@ z <- as.character(x)
 expect_equal(minmax(x), c(min(x), max(x)))
 expect_equal(minmax(y), c(min(y), max(y)))
 expect_equal(minmax(z), c(min(z), max(z)))
+expect_identical(minmax(NULL), NULL)
+expect_identical(minmax(integer(0), empty_result = c(Inf, -Inf)), c(Inf, -Inf))
+expect_identical(minmax(new.env()), NULL)
 
 
