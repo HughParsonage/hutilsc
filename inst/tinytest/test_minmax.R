ab9bfe9b61fff8ec43e29bb109f6e92f7392f7fb
HughParsonage <hugh.parsonage@gmail.com>
2021-02-09 00:27:12 +1100

    Add test

--- inst/tinytest/test_minmax.R
+++ inst/tinytest/test_minmax.R
@@ -1,11 +1,13 @@
 
-x <- c(5L, 2L, 1L, 99L)
+x <- c(5L, 2L, 1L, 99L, 5L)
 y <- as.double(x)
 z <- as.character(x)
+r <- as.raw(x)
 
 expect_equal(minmax(x), c(min(x), max(x)))
 expect_equal(minmax(y), c(min(y), max(y)))
 expect_equal(minmax(z), c(min(z), max(z)))
+# expect_equal(minmax(r), r[c(which.min(r), which.max(r))])
 expect_identical(minmax(NULL), NULL)
 expect_identical(minmax(integer(0), empty_result = c(Inf, -Inf)), c(Inf, -Inf))
 expect_identical(minmax(new.env()), NULL)
@@ -14,6 +16,7 @@ expect_identical(minmax(new.env()), NULL)
 expect_equal(wminmax(x), c(which.min(x), which.max(x)))
 expect_equal(wminmax(y), c(which.min(y), which.max(y)))
 expect_equal(wminmax(z), c(which.min(z), which.max(z)))
+# expect_equal(wminmax(r), c(which.min(r), which.max(r)))
 
 
 
