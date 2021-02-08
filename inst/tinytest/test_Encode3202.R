a32577b33664e71d58653bc202aa1db920c057b5
HughParsonage <hugh.parsonage@gmail.com>
2021-02-09 01:43:31 +1100

    Add coverage

--- inst/tinytest/test_Encode3202.R
+++ inst/tinytest/test_Encode3202.R
@@ -1,15 +1,19 @@
 
+Validate3202 <- hutilsc:::Validate3202
+Decode3202 <- hutilsc:::Decode3202
+Encode3202 <- hutilsc:::Encode3202
+
 x <- c("320200000002", "320201000000")
 expect_true(Validate3202(x))
 expect_equal(Decode3202(Encode3202(x)), x)
 y <- c("320200000002", "420201000000")
 expect_error(Validate3202(y))
-expect_error(Validate3202("1234567890"))
+expect_error(Validate3202("12345678901"))
 
 
 expect_equal(Lookup4(names2int("Hugh", "Parsonage")), "HuPa")
 
 tensc <- as.character(tens <- as.integer(10^(0:7)))
-expect_equal(pad0(tensc, 9),
-             formatC(tens, digits = 9, flag = "0", format = "d"))
+expect_equal(hutilsc:::pad0(tensc, 9L),
+             formatC(tens, digits = 8, flag = "0", format = "d"))
 
--- inst/tinytest/test_decoder.R
+++ inst/tinytest/test_decoder.R
@@ -2,3 +2,13 @@ rid <- c("5002P00000DkX9tQAF", "5002P00000DqXJKQA3", "1750602", "5002P00000DqCtS
          "2135143")
 
 expect_equal(hutilsc:::DecodeRecordID(hutilsc:::EncodeRecordID(rid)), rid)
+
+counts <- hutilsc:::CountRecordID(c("5002P00000DkX9tQAF", "5002P00000DkX9tQAF"))
+expect_equal(counts, c(rep(1L, length(counts) - 1L), 0L))
+
+counts2 <- hutilsc:::CountRecordID(c("5002P00000DkX9tQAF", "5002P00000DkX9tQAG"))
+expect_equal(counts2, c(rep(1L, length(counts) - 2L), 2L, 0L))
+
+counts3 <- hutilsc:::CountRecordID(c("5002P00000DkX9tQAF", "5002P00000DkX9tQAG", "5002P00000DkX9tQBZ"))
+expect_equal(counts3, c(rep(1L, length(counts) - 3L), 2:3, 0L))
+
