6ec872a8d6d61e11408a4161e82a4ee4c056e4bb
HughParsonage <hugh.parsonage@gmail.com>
2021-03-01 20:00:33 +1100

    Don't run test in 32bit mode

--- inst/tinytest/test_minmax.R
+++ inst/tinytest/test_minmax.R
@@ -20,8 +20,9 @@ expect_equal(wminmax(z), c(which.min(z), which.max(z)))
 l <- c(TRUE, FALSE)
 expect_equal(wminmax(l), c(which.min(l), which.max(l)))
 rm(x, y, z, r, l)  # need max memory for tomorrow
-x <- raw(2^31)
-expect_equal(wminmax(x), c(1L, 1L))
-
+if (is64bit()) {
+  x <- raw(2^31)
+  expect_equal(wminmax(x), c(1L, 1L))
+}
 
 
