b8ba01e818c423505a3b6d7d9eb124970e83c330
HughParsonage <hugh.parsonage@gmail.com>
2021-03-04 01:37:40 +1100

    Complete coverage

--- inst/tinytest/test_test_input_types.R
+++ inst/tinytest/test_test_input_types.R
@@ -1,4 +1,9 @@
 
 # Hardcoded for switch statements
 expect_equal(hutilsc:::Ctest_input_types(1:5, 1:5, 1:5, 1:5), 1L)
+expect_equal(hutilsc:::Ctest_input_types(1:5, 1:5, 1:5, 1:5), 1L)
 expect_equal(hutilsc:::Ctest_input_types(1L, 1L, 1L, 1L), 171L)
+expect_equal(hutilsc:::Ctest_input_types(1:5, 1L, 1:5, 1L), 11L)  # IiIi
+expect_equal(hutilsc:::Ctest_input_types(1:5, pi, 1:5, pi), 16L)  # IrIr
+
+expect_equal(hutilsc:::Ctest_input_types(1:5 + 0, pi, 1L, 1L), 111L)  # Rrii
