affde28b6643d2e9ce54b5045cb668d0958fd76e
HughParsonage <hugh.parsonage@gmail.com>
2021-03-24 13:49:20 +1100

    Use fastmatch to be competitive with igraph

--- mec.R
+++ mec.R
@@ -18,14 +18,21 @@ for (o in 4:6) {
   ego_net(1L, order = o, TriangleEdges)
 }
 for (i in 1:100) {
-  Edges <- fread("~/dhhs/dhhs-names/Edges.tsv", key = "from,to")
-  cat(s <- sample(Edges$from, size = 1))
+  Edges <- fread("~/dhhs/dhhs-names/Edges.tsv", key = "orig,dest")
+  cat(s <- sample(Edges$orig, size = 1))
   ego_net(s, order = 6L, Edges = Edges)
   cat(".")
   ego_net(NULL, order = 6L, Edges = Edges)
 }
-u <- Edges[, union(from, to)]
+u <- Edges[, union(orig, dest)]
 bench::system_time(sapply(u, ego_net))
+
+Edges_E <- fread("~/dhhs/dhhs-names/egoFail-2021-03-23.tsv", key = "orig,dest")
+# 25442979
+Cego <- ego_net(21L, order = 2L, Edges = Edges_E)
+Bego <- basic_ego_net(21L, order = 2L, Edges = Edges_E)
+
+
 
 cat("Complete\n")
 
