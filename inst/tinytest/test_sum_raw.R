e6e8af00cb114015243dbed852f08b795b56cf86
HughParsonage <hugh.parsonage@gmail.com>
2021-06-16 10:58:43 +1000

    sum/count_raw

---
+++ inst/tinytest/test_sum_raw.R
@@ -0,0 +1,12 @@
+library(hutilsc)
+library(tinytest)
+
+x <- raw(5)
+expect_equal(sum_raw(x), 0)
+x <- c(as.raw(0), as.raw(1), as.raw(0))
+expect_equal(sum_raw(x), 1)
+expect_equal(sum_raw(charToRaw('a')), utf8ToInt('a'))
+out <- integer(256)
+out[2] <- 1L
+expect_equal(tabulate_raw(as.raw(1L)), out)
+
